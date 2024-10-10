8094be9c94ae886b09e86c7613fd0cd2c0ace135
Naman Pandey <109365773+Namanlko@users.noreply.github.com>
2024-10-10 07:53:00 +0530

    Chapter 3 Completed

---
+++ Chapter 3 - Strings/EscapeSequence.py
@@ -0,0 +1,8 @@
+# Escape Sequnce Character: Sequence of characters after backslash '\' -> Escape Sequence Charaters.
+
+# Escape Sequence Character comprises of more than one characters but represents one character when used within the strings.
+
+# Examples: \n, \t, \', \\ etc.
+
+print("Hello\nWorld")
+print("Dear\n\tAngela Yu")
---
+++ Chapter 3 - Strings/String.py
@@ -0,0 +1,10 @@
+# String: String is a data type in Python. String is enclosed in (single, double or triple quotes) quotes.
+# We can primarily, write a string in these three ways
+
+a = 'Angela Yu'
+b = "Angela Yu"
+c = '''Angela Yu'''
+
+print(a)
+print(b)
+print(c)
