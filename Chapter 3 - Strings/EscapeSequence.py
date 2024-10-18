7e9089fd901fa62901c3303c5d7cf51ede8b91e4
Naman Pandey <109365773+Namanlko@users.noreply.github.com>
2024-10-19 00:14:35 +0530

    Delete Chapter 3 - Strings directory

--- Chapter 3 - Strings/EscapeSequence.py
+++
@@ -1,8 +0,0 @@
-# Escape Sequnce Character: Sequence of characters after backslash '\' -> Escape Sequence Charaters.
-
-# Escape Sequence Character comprises of more than one characters but represents one character when used within the strings.
-
-# Examples: \n, \t, \', \\ etc.
-
-print("Hello\nWorld")
-print("Dear\n\tAngela Yu")
